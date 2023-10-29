cfbd75672ca9941947f76c4e06b667a940b662d1
Chris Whittle <chris.whittle@jbhunt.com>
2023-10-29 18:32:55 -0500

    changed timing to fix the speed issue

--- main.ts
+++ main.ts
@@ -4,24 +4,20 @@ const { remote } = require('electron');
 
 export default class TaskCount extends Plugin {
 	async onload() {
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => this.getSearch(), 5000));
+		this.registerInterval(window.setInterval(this.getSearch, 20000));
 		remote.app.dock.setBadge('');
-		this.getSearch();
-
+		setTimeout(this.getSearch, 2000);
 	}
 	async onunload() {
 		remote.app.dock.setBadge('')
 	}
 	async getSearch() {
 		const dataviewApi = getDataviewAPI();
-		const taskQuery = `TABLE WITHOUT ID Tasks.path
+		const taskQuery = `TABLE Tasks.path
 		WHERE file.tasks
 		FLATTEN file.tasks AS Tasks
 		WHERE Tasks.text != "" AND !contains(Tasks.path, "TPL") AND !Tasks.completed AND (Tasks.due = null OR date(Tasks.due) < date("tomorrow"))`;
 		var dataviewResults = await dataviewApi.tryQuery(taskQuery);
-
 		remote.app.dock.setBadge(dataviewResults.values.length > 0 ? dataviewResults.values.length.toString() : '')
-		dataviewResults = null;
 	}
 }
